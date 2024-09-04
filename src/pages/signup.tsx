d3099f8982d381db23c7020a9eddb85e386eaa71
gitmayoo <164817553+gitmayoo@users.noreply.github.com>
2024-09-04 13:23:24 +0530

    signup

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -2,13 +2,19 @@ import { Button, Card } from "../components/Card";
 
 export default function Signup() {
   return (
+    <div className="h-lvh w-lvw flex justify-center items-center">
+     
     <Card>
-      <div className="flex flex-col h-[4x00px] justify-center ">
+    <div className="flex flex-col h-[500px] justify-center items-center ">
+    <h1 className="mb-5">
+        Register Account
+      </h1>
         <form className="max-w-sm mx-auto">
-          <div className="mb-5">
+          <div className="mb-2">
             <label
               htmlFor="username"
-              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              // border-green-500 text-green-900 dark:text-green-400 placeholder-green-700 dark:placeholder-green-500 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:bg-gray-700 dark:border-green-500
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white "
             >
               Your username
             </label>
@@ -20,7 +26,23 @@ export default function Signup() {
               required
             />
           </div>
-          <div className="mb-5">
+          <div className="mb-2">
+            <label
+              htmlFor="phonenumber"
+              // border-green-500 text-green-900 dark:text-green-400 placeholder-green-700 dark:placeholder-green-500 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:bg-gray-700 dark:border-green-500
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white "
+            >
+              Your phone number
+            </label>
+            <input
+              type="text"
+              id="phonenumber"
+              className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+              
+              required
+            />
+          </div>
+          <div className="mb-2">
             <label
               htmlFor="password"
               className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
@@ -34,7 +56,7 @@ export default function Signup() {
               required
             />
           </div>
-          <div className="mb-5">
+          <div className="mb-2">
             <label
               htmlFor="repeat-password"
               className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
@@ -48,38 +70,26 @@ export default function Signup() {
               required
             />
           </div>
-          <div className="flex items-start mb-5">
-            <div className="flex items-center h-5">
-              <input
-                id="terms"
-                type="checkbox"
-                value=""
-                className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800"
-                required
-              />
-            </div>
-            <label
-              htmlFor="terms"
-              className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-            >
-              I agree with the{" "}
-              <a
-                href="#"
-                className="text-blue-600 hover:underline dark:text-blue-500"
-              >
-                terms and conditions
-              </a>
-            </label>
-          </div>
+          
           <button
             type="submit"
-            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 mt-5"
           >
             Register new account
           </button>
+          <div className="flex items-start mb-2">
+          <p className="mt-1 text-center text-sm text-gray-500">
+    Already have an account, 
+      <a href="/login" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"> Login</a>
+    </p>
+          
+           
+          </div>
         </form>
       </div>
     </Card>
+    </div>
+
   );
 }
 
